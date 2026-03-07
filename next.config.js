3217d6b73e5b439ad51d45432f3cb847b501778f
Umesh <umesh@fundamento.ai>
2026-03-07 16:59:45 +0530

    add config file for static page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't have a server to optimize images
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
